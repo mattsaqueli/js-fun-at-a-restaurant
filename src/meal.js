a03104362dd3af6df974ee4ca457c4659a5633e8
Matt Saqueli <msaqueli22@turing.edu>
2023-02-03 14:51:25 -0600

    Complete all tasks in this file

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
-
+function nameMenuItem(foodType) {
+  return `Delicious ${foodType}`;
 }
 
+function createMenuItem(menuItemName, foodPrice, mealType) {
+  var menuItem = {
+    name: menuItemName,
+    price: foodPrice,
+    type: mealType,
+  } 
+  return menuItem;
+}
+
+function addIngredients(cheese, ingredients) {
+  if (!ingredients.includes(cheese)) {
+    ingredients.push(cheese);
+  }
+} 
+
+function formatPrice(price) {
+  var formattedPrice = (`$${price}`);
+  return formattedPrice;
+}
+
+function decreasePrice(price) {
+   var price = 5.4;
+   return price;
+}
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
