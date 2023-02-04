902505419a7072f80f8cffaed305e5a9246a35b3
Matt Saqueli <msaqueli22@turing.edu>
2023-02-04 12:56:28 -0600

    Complete all tasks

--- src/order.js
+++ src/order.js
@@ -1,7 +1,38 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(order)
+  }
+  return deliveryOrders;
+}
+
+function refundOrder(refund, orders) {
+  for (i = 0; i < orders.length; i++) {
+    if (orders[i].orderNumber === refund) {
+      return orders.splice([i], 1)
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  var items = []
+  for (i = 0; i < deliveryOrders.length; i++) {
+    items.push(deliveryOrders[i].item)
+  }
+  return items.join(', ');
+}
+
+function searchOrder(deliveryOrders, itemName) {
+  for (i = 0; i < deliveryOrders.length; i++)
+    if (deliveryOrders[i].item === itemName) {
+    return true
+    }
+    return false
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
